2fc35060a93a614367339328ffad7b9f83e22a8e
Bunbee <barnabmutinhiri@gmail.com>
2021-10-28 09:32:12 +0200

    Add functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import './style.css';
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported successfully
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
